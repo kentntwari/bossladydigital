4f488fb66438ec60bfae199e6d00226c8cee8dce
Kent Ntwari <designerjehovah@gmail.com>
2024-04-07 15:31:53 -0400

    chore:configure nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  build: {
+    transpile: ["trpc-nuxt"],
+  },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/kinde", "@vueuse/nuxt"],
+});
