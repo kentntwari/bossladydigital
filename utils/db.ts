42bd94af46d6428f1291f054d6cd8215eae396bb
Kent Ntwari <designerjehovah@gmail.com>
2024-04-13 14:14:38 -0400

    update db queries

--- utils/db.ts
+++ utils/db.ts
@@ -3,11 +3,136 @@ import { Prisma } from "@prisma/client";
 import prisma from "./prisma";
 import { zodProductSchema } from "./zodSchemas";
 
+export async function getSessionFromDb(sessionId: string) {
+  try {
+    const session = await prisma.session.findUnique({
+      where: {
+        stripeSessionId: sessionId,
+      },
+    });
+
+    await prisma.$disconnect();
+    return session;
+  } catch (error) {
+    logErrors(error);
+  }
+}
+
+export async function getOrdersBySession(sessionId: string, userEmail: string) {
+  try {
+    const currentSession = await getSessionFromDb(sessionId);
+
+    const currentUser = await prisma.user.findFirst({
+      where: {
+        email: userEmail,
+      },
+    });
+
+    console.log(userEmail);
+
+    if (!currentUser) throw new Error("User not found");
+
+    if (!currentSession) throw new Error("Session not found");
+
+    const orders = await prisma.order.findFirst({
+      where: {
+        id: currentSession.orderId,
+        userId: currentUser.id,
+      },
+      select: {
+        product: true,
+      },
+    });
+
+    if (!orders) throw new Error("Order not found");
+
+    await prisma.$disconnect();
+    return orders;
+  } catch (error) {
+    logErrors(error);
+  }
+}
+
+export async function updateSessionViewedState(sessionId: string) {
+  try {
+    const session = await prisma.session.findUnique({
+      where: {
+        stripeSessionId: sessionId,
+      },
+      select: {
+        hasBeenViewed: true,
+      },
+    });
+
+    if (!session) throw new Error("Session not found");
+
+    await prisma.session.update({
+      where: {
+        stripeSessionId: sessionId,
+      },
+      data: {
+        hasBeenViewed: !session.hasBeenViewed ? true : false,
+      },
+    });
+
+    await prisma.$disconnect();
+    return;
+  } catch (error) {
+    logErrors(error);
+  }
+}
+
 export async function getProducts() {
   try {
     const products = await prisma.product.findMany();
     await prisma.$disconnect();
     return products;
+  } catch (error) {
+    logErrors(error);
+  }
+}
+
+export async function getCustomerOrders(email: string) {
+  try {
+    const orders = await prisma.$transaction(async (tx) => {
+      const customer = await tx.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+
+      if (!customer) throw new Error("Customer not found");
+
+      const purchasedProducts = await tx.order.findMany({
+        where: {
+          userId: customer.id,
+        },
+        select: {
+          productId: true,
+        },
+      });
+
+      return await Promise.all(
+        purchasedProducts.map(
+          async ({ productId }) =>
+            await tx.product.findUnique({
+              where: {
+                id: productId,
+              },
+              select: {
+                name: true,
+                price: true,
+                currency: true,
+                description: true,
+                checklistUrl: true,
+              },
+            })
+        )
+      );
+    });
+
+    await prisma.$disconnect();
+    return orders;
   } catch (error) {
     logErrors(error);
   }
@@ -27,6 +152,81 @@ export async function createNewProduct(
         stripePriceId: product.price.id,
         checklistUrl: product.url,
       },
+    });
+
+    await prisma.$disconnect();
+    return;
+  } catch (error) {
+    logErrors(error);
+  }
+}
+
+export async function createOrder(
+  userName: string,
+  userEmail: string,
+  productId: string,
+  sessionId: string,
+  isPaymentDone: boolean
+) {
+  try {
+    await prisma.$transaction(async (tx) => {
+      //find user
+      const currentUser = await tx.user.findUnique({
+        where: { email: userEmail },
+      });
+
+      const productPurchased = await tx.product.findFirst({
+        where: {
+          stripeProductId: productId,
+        },
+      });
+
+      const payment = await tx.payment.create({
+        data: {
+          isDone: isPaymentDone,
+        },
+      });
+
+      if (!productPurchased) throw new Error("Product not found");
+
+      if (!currentUser) {
+        const newUser = await tx.user.create({
+          data: {
+            name: userName,
+            email: userEmail,
+          },
+        });
+
+        return await tx.order.create({
+          data: {
+            userId: newUser.id,
+            productId: productPurchased.id,
+            paymentId: payment.id,
+            session: {
+              create: {
+                stripeSessionId: sessionId,
+                paymentId: payment.id,
+              },
+            },
+          },
+        });
+      }
+
+      if (currentUser) {
+        return await tx.order.create({
+          data: {
+            userId: currentUser.id,
+            productId: productPurchased.id,
+            paymentId: payment.id,
+            session: {
+              create: {
+                stripeSessionId: sessionId,
+                paymentId: payment.id,
+              },
+            },
+          },
+        });
+      }
     });
 
     await prisma.$disconnect();
