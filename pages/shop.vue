3eb665e1197d2bf9ef277a2b56f5f001f70c1bd5
Kent Ntwari <designerjehovah@gmail.com>
2024-04-13 14:11:16 -0400

    feat:update shop page

--- pages/shop.vue
+++ pages/shop.vue
@@ -1,9 +1,54 @@
 <script lang="ts" setup>
+  const { $client } = useNuxtApp();
 
+  const { data: products } = useAsyncData("products", () =>
+    $client.getProducts.query()
+  );
 </script>
 
 <template>
-  <section class="px-5">
-    <h1 class="uppercase text-xl">Products</h1>
+  <section class="space-y-12">
+    <h1 class="ml-5 uppercase text-xl">Products</h1>
+    <div class="grid grid-cols-1">
+      <template v-if="products">
+        <article
+          v-for="(product, index) in products"
+          :key="product.id"
+          class="px-5 py-6 space-y-10 border-[#1a1a1a]"
+          :class="[
+            index === products.length - 1 ? 'border-t border-b' : 'border-t',
+          ]"
+        >
+          <div class="flex flex-col gap-4">
+            <p class="font-bold uppercase text-base">{{ product.name }}</p>
+            <span class="text-base">{{ product.description }}</span>
+          </div>
+
+          <div class="w-full flex items-center justify-between">
+            <span class="text-base">${{ +product.price / 100 }}</span>
+            <Button
+              type="button"
+              @click="
+                async (event) => {
+                  if (!$auth.loggedIn)
+                    return navigateTo('/api/login', { external: true });
+
+                  const session = await $client.createStripeSession.mutate({
+                    priceId: product.stripePriceId,
+                    currentUser: {
+                      name: `${$auth.user.given_name} ${$auth.user.family_name}`,
+                      email: $auth.user.email,
+                    },
+                  });
+
+                  return await navigateTo(session.url, { external: true });
+                }
+              "
+              >purchase</Button
+            >
+          </div>
+        </article>
+      </template>
+    </div>
   </section>
 </template>
